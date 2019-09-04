e50cd3a964700bcd119b441282f87c598075ce70
CandaceRossi <50240571+CandaceRossi@users.noreply.github.com>
2019-09-04 17:53:54 -0400

    built out the two class components, just linking the form still and saving handlechange

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
-import React from 'react';
+import React from "react";
+import TodoList from "./components/TodoComponents/TodoList";
 
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      value: ""
+    };
+  }
+  onChange = e => {
+    this.setState({ value: e.target.value });
+  };
+
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
   render() {
     return (
       <div>
-        <h2>Welcome to your Todo App!</h2>
+        <h2>Todo List: MVP!</h2>
       </div>
+      <TodoList />
     );
   }
 }
--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -1,2 +1,24 @@
+import React from "react";
+
 // your components will all go in this `component` directory.
 // feel free to change this component.js into TodoList.js
+class TodoList extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      value: ""
+    };
+  }
+  render() {
+    return( 
+        <div>
+
+
+
+        </div>;         
+    )
+  }
+}
+
+export default TodoList;
