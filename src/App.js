8b9be148b46821eb13c40075ee3f257e62a8a647
CandaceRossi <50240571+CandaceRossi@users.noreply.github.com>
2019-09-30 17:58:05 -0400

    set up files

--- src/App.js
+++ src/App.js
@@ -1,28 +1,79 @@
 import React from "react";
-import TodoList from "./components/TodoComponents/TodoList";
+import TodoList from "./components/TodoComponents/TodoList.js";
+import TodoForm from "./components/TodoComponents/TodoForm.js";
+import { data } from "./dummyData.js";
 
 class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      value: ""
+      todos: []
     };
   }
-  onChange = e => {
-    this.setState({ value: e.target.value });
+  componentDidMount = () => {
+    const localData = JSON.parse(localStorage.getItem("todolist"));
+    this.setState({
+      todos: data
+    });
   };
 
-  // you will need a place to store your state in this component.
-  // design `App` to be the parent component of your application.
-  // this component is going to take care of state, and any change handlers you need to work with your state
+  handleSubmit = (event, newTodo) => {
+    event.preventDefault();
+    let todoShape = {
+      task: newTodo,
+      id: Date.now(),
+      completed: false
+    };
+
+    const updatedList = [...this.state.todos, todoShape];
+
+    this.setState({
+      todos: updatedList
+    });
+    localStorage.setItem("todolist", JSON.stringify(updatedList));
+  };
+
+  toggleCompleted = id => {
+    const TodoById = this.state.todos.map(todo => {
+      return todo.id === id ? { ...todo, completed: !todo.completed } : todo;
+    });
+    this.setState({
+      todos: TodoById
+    });
+    localStorage.setItem("todolist", JSON.stringify(TodoById));
+  };
+
+  clearCompleted = () => {
+    const completed = this.state.todos.filter(todo => {
+      return todo.completed === false;
+    });
+    this.setState({
+      todos: completed
+    });
+    localStorage.setItem("todolist", JSON.stringify(completed));
+  };
+
   render() {
+    if (!this.state.todos) return <h1>Loading Todos</h1>;
     return (
       <div>
-        <h2>Todo List: MVP!</h2>
+        <h2>Welcome to your Todo App!</h2>
+        <TodoForm
+          handleSubmit={this.handleSubmit}
+          clearCompleted={this.clearCompleted}
+        />
+        <TodoList
+          todolist={this.state.todos}
+          toggleCompleted={this.toggleCompleted}
+        />
       </div>
-      <TodoList />
     );
   }
 }
 
 export default App;
+
+// you will need a place to store your state in this component.
+// design `App` to be the parent component of your application.
+// this component is going to take care of state, and any change handlers you need to work with your state
+//
---
+++ src/components/TodoComponents/Todo.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "./Todo.css";
+
+class Todo extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        <li
+          className={this.props.todoitem.completed ? "completed" : null}
+          onClick={() => this.props.toggleCompleted(this.props.todoitem.id)}
+        >
+          {this.props.todoitem.task}
+        </li>
+      </div>
+    );
+  }
+}
+
+export default Todo;
